d2e0a0fa0b353a3b6ea8e82a0c13d93ff278f319
ganapathy <100988867+r-ganapathy@users.noreply.github.com>
2025-06-27 17:29:18 +0530

    swagger remove

--- Program.cs
+++ Program.cs
@@ -24,8 +24,11 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 if (app.Environment.IsDevelopment())
 {
